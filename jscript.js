1a38f875bd3a6f69e0684e7f7e94fb65f75a3fbc
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2024-11-07 20:45:57 +0100

    refactor the code and implementing the backend of the popup button

--- jscript.js
+++ jscript.js
@@ -1,24 +1,46 @@
 
+// implementing the popup button
 
+const button = document.getElementById('popup');
 
-
-const grid = document.querySelector('.grid');
-// Use a nested for loop to create the grid cells
-for (let i = 0; i < 10; i++) {
-  for (let j = 0; j < 10; j++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    grid.appendChild(cell);
+// Add an event listener to the button that listens for the click event
+button.addEventListener('click', function() {
+  // Display the prompt when the button is clicked
+  const userInput = prompt('Please enter the number of squares per side for the new grid:');
+  if (userInput < 100 && userInput > 0){
+    createGridCells(userInput);
   }
+  else {
+    alert("number failed");
+  }
+});
+
+
+
+function createGridCells (number) {
+    const grid = document.querySelector('.grid');
+    // Use a nested for loop to create the grid cells
+    for (let i = 0; i < number; i++) {
+    for (let j = 0; j < number; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        grid.appendChild(cell);
+    }
+    }
+
+    hoverOver();
+}    
+
+
+function hoverOver (){
+    // hover over function
+    const cell = document.querySelectorAll(".cell")
+
+    cell.forEach(function(myBox) {
+    //const color = window.getComputedStyle(myBox).getPropertyValue('background-color'); //grabs the css background color
+    myBox.addEventListener('mouseover', (e) => { 
+        e.target.style.backgroundColor = 'black';
+    });
+    });
 }
 
-
-// hover over function
-const cell = document.querySelectorAll(".cell")
-
-cell.forEach(function(myBox) {
-  //const color = window.getComputedStyle(myBox).getPropertyValue('background-color'); //grabs the css background color
-  myBox.addEventListener('mouseover', (e) => { 
-    e.target.style.backgroundColor = 'black';
-});
-});
